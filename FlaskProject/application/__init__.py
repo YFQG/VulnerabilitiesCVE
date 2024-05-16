dbc7400a521eff481ea32605d1fe149ceb50a547
unknown <rechoinfomauroyid@gmail.com>
2024-05-15 20:34:45 -0500

    Delete unused models.py and mongoDB connection

--- FlaskProject/application/__init__.py
+++ FlaskProject/application/__init__.py
@@ -3,15 +3,4 @@ from pymongo import MongoClient
 
 app = Flask(__name__)
 
-# Configura la conexión a MongoDB
-# Reemplaza 'nombre_de_usuario', 'contraseña', 'nombre_de_host' y 'puerto' con tus propias credenciales y detalles de conexión
-client = MongoClient('mongodb+srv://pipequiroga34:fRn70Ys7zX88KCxO@cluster0.h2lbkt0.mongodb.net/?retryWrites=true&w=majority&appName=Cluster0')
-
-# Selecciona la base de datos
-db = client.get_database('Vulnerabilities')  # Cambia 'nombre_de_tu_base_de_datos' por el nombre de tu base de datos
-
-collection = db['PcSoftwareCVE']
-
-db = collection.find()
-
 from . import routes
